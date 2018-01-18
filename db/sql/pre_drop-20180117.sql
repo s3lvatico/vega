bd55818fb01ff9da5aa7306c0ba0a0e9c34c93c9
S3LvAtiCo <s3lvatico@gmail.com>
2018-01-18 22:52:11 +0100

    un sacco di modifiche ma c'è ancora molto da fare

---
+++ db/sql/pre_drop-20180117.sql
@@ -0,0 +1,92 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: sandbox
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `vg_allergene`
+--
+
+DROP TABLE IF EXISTS `vg_allergene`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vg_allergene` (
+  `id` char(36) NOT NULL,
+  `id_categoria` char(36) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000',
+  `nome` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_allergene_nome` (`nome`),
+  KEY `fk_allergene__categoria_id_idx` (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vg_categoria`
+--
+
+DROP TABLE IF EXISTS `vg_categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vg_categoria` (
+  `id` char(36) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nome_UNIQUE` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vg_report`
+--
+
+DROP TABLE IF EXISTS `vg_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vg_report` (
+  `id` char(36) NOT NULL,
+  `data_creazione` date NOT NULL,
+  `nome_soggetto` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vg_report_data`
+--
+
+DROP TABLE IF EXISTS `vg_report_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vg_report_data` (
+  `id_report` char(36) NOT NULL,
+  `id_allergene` char(36) NOT NULL,
+  `tox` decimal(3,2) NOT NULL,
+  PRIMARY KEY (`id_report`,`id_allergene`),
+  KEY `fk_report_data__id_allergene_idx` (`id_allergene`),
+  CONSTRAINT `fk_report_data__id_allergene` FOREIGN KEY (`id_allergene`) REFERENCES `vg_allergene` (`id`) ON UPDATE NO ACTION,
+  CONSTRAINT `fk_report_data__id_report` FOREIGN KEY (`id_report`) REFERENCES `vg_report` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-17 11:10:42
