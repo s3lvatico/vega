33f770e8a217d185e2e17a2ee5197972291e8e71
S3lvatico <s3lvatico@gmail.com>
2018-05-20 15:19:46 +0200

    refactor: nomi in inglese

---
+++ src/main/java/org/gmnz/vega/domain/ToxicityRating.java
@@ -0,0 +1,38 @@
+package org.gmnz.vega.domain;
+
+
+public class ToxicityRating {
+
+	private Allergen allergen;
+
+	private double toxicity;
+
+
+
+	public ToxicityRating(Allergen a, double toxicity) {
+		allergen = a;
+		this.toxicity = toxicity;
+	}
+
+
+
+	public Allergen getAllergen() {
+		return allergen;
+	}
+
+
+
+	public double getToxicity() {
+		return toxicity;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "ToxicityRating{" +
+				"allergen=" + allergen +
+				", toxicity=" + toxicity +
+				'}';
+	}
+}
