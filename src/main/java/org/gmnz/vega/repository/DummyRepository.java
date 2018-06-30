4ab6b00d020cd1ad6fb7a9ebf74ac3fd935067e0
S3lvatico <s3lvatico@gmail.com>
2018-06-30 16:06:02 +0200

    aggiunti dati di esempio per categorie ed allergeni (in relazione)

--- src/main/java/org/gmnz/vega/repository/DummyRepository.java
+++ src/main/java/org/gmnz/vega/repository/DummyRepository.java
@@ -2,6 +2,7 @@ package org.gmnz.vega.repository;
 
 
 import org.gmnz.vega.domain.Allergen;
+import org.gmnz.vega.domain.Category;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -10,16 +11,73 @@ import java.util.Set;
 
 public final class DummyRepository {
 
-	private static final Set<Allergen> db = new HashSet<>();
+	private static final Set<Allergen> ALLERGENS = new HashSet<>();
+	private static final Set<Category> CATEGORIES = new HashSet<>();
 
 	static {
-		db.add(new Allergen("Farina"));
-		db.add(new Allergen("Lievito"));
-		db.add(new Allergen("Alcol"));
-		db.add(new Allergen("Carne bovina"));
+
+		Category c = new Category("carni");
+		Allergen a = new Allergen("manzo");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
+		CATEGORIES.add(c);
+
+		a = new Allergen("pollo");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
+
+		a = new Allergen("maiale");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
+
+		c = new Category("cereali");
+		CATEGORIES.add(c);
+
+		a = new Allergen("avena");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
+
+		a = new Allergen("farina");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
+
+		a = new Allergen("mais");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
+
+		a = new Allergen("orzo");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
+
+
+		c = new Category("condimenti");
+		CATEGORIES.add(c);
+
+		a = new Allergen("lievito");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
+
+		a = new Allergen("olio di oliva");
+		a.setCategory(c);
+		c.add(a);
+		ALLERGENS.add(a);
 	}
 
 	public static Collection<Allergen> getRegisteredAllergens() {
-		return db;
+		return ALLERGENS;
+	}
+
+
+
+	public static Collection<Category> getRegisteredCategories() {
+		return CATEGORIES;
 	}
 }
