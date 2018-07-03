88209ef987b9ad0cbadf6eb7edfcc9268c29d691
S3lvatico <s3lvatico@gmail.com>
2018-07-03 17:36:59 +0200

    GSha1.java : completato metodo computeSha1AsString

--- src/main/java/org/gmnz/sandbox/GSha1.java
+++ src/main/java/org/gmnz/sandbox/GSha1.java
@@ -11,16 +11,19 @@ class GSha1 {
 
 
 
+
 	GSha1() {
 		MessageDigest sha1digest1;
 		try {
 			sha1digest1 = MessageDigest.getInstance("SHA1");
-		} catch (NoSuchAlgorithmException e) {
+		}
+		catch (NoSuchAlgorithmException e) {
 			System.err.println("warning - could not initialize the digester");
 			sha1digest1 = null;
 		}
 		sha1digest = sha1digest1;
 	}
+
 
 
 
@@ -33,15 +36,34 @@ class GSha1 {
 
 
 
+
 	String computeSha1AsString(String s) {
 		byte[] digest = computeSha1(s);
-		return "to be implemented";
+		StringBuilder sbDigest = new StringBuilder();
+		for (byte b : digest) {
+			// int n = (int) (b & 0xff);
+			sbDigest.append(String.format("%02x", b & 0xff));
+		}
+		return sbDigest.toString();
 	}
 
 
 
+
 	private void validateDigester() {
-		if (sha1digest == null) throw new UnsupportedOperationException("digester not initialized");
+		if (sha1digest == null)
+			throw new UnsupportedOperationException("digester not initialized");
+	}
+
+
+
+
+	public static void main(String[] args) {
+		GSha1 sha1 = new GSha1();
+		String c64Sha = sha1.computeSha1AsString("c64");
+		// b865271afeded8795c19b5a63716c3d16e6ffca3
+		System.out.println(c64Sha);
+		// b865271afeded8795c19b5a63716c3d16e6ffca3
 	}
 
 }
