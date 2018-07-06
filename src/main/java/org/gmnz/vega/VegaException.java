6d72d95e3ba0f20ac9981357464c15a6c6b5d1f3
Simone Monotti <s3lvatico@gmail.com>
2018-07-06 12:28:46 +0200

    aggunta VegaException.java

---
+++ src/main/java/org/gmnz/vega/VegaException.java
@@ -0,0 +1,14 @@
+
+package org.gmnz.vega;
+
+public class VegaException extends Exception {
+
+	private static final long serialVersionUID = 5203809160051535474L;
+
+
+
+	public VegaException(String message) {
+		super(message);
+	}
+
+}
