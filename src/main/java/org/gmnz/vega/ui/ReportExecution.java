ba352d71103148826eff4f5d17d3f20e4377853f
S3lvatico <s3lvatico@gmail.com>
2018-07-15 16:33:23 +0200

    creazione report - da testare

--- src/main/java/org/gmnz/vega/ui/ReportExecution.java
+++ src/main/java/org/gmnz/vega/ui/ReportExecution.java
@@ -1,8 +1,99 @@
 package org.gmnz.vega.ui;
 
 
+import org.gmnz.vega.Vega;
+import org.gmnz.vega.VegaException;
+import org.gmnz.vega.VegaImpl;
+import org.gmnz.vega.base.VegaUtil;
+import org.gmnz.vega.domain.Allergen;
+import org.gmnz.vega.domain.Report;
+import org.gmnz.vega.domain.ToxicityRating;
+import org.gmnz.vega.service.AllergenService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Enumeration;
+
+
 /**
  * creato da simone in data 12/07/2018.
  */
-public class ReportExecution {
+public class ReportExecution extends HttpServlet {
+
+	private Vega vega;
+
+
+
+	@Override
+	public void init() {
+		vega = new VegaImpl();
+	}
+
+
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String action = req.getParameter("action");
+		if (action == null || action.isEmpty()) {
+			throw new ServletException("no action specified");
+		}
+		// System.out.format("action requested: <%s>%n", action);
+		executeAction(action, req, resp);
+	}
+
+
+
+	private void executeAction(String action, HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			switch (action) {
+				case Action.CREATE:
+					String subjectName = req.getParameter("subjectName");
+					if (VegaUtil.stringNullOrEmpty(subjectName)) {
+						resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "A subject must be specified");
+						return;
+					}
+					createReport(subjectName, req, resp);
+					break;
+
+				case Action.DELETE:
+					// TODO fare
+					break;
+				default:
+					throw new ServletException("invalid action specified");
+			}
+		} catch (VegaException ve) {
+			String errorMessage = String.format("exception thrown while executing action -- %s :: %s",
+					ve.getClass().getName(), ve.getMessage());
+			throw new ServletException(errorMessage, ve);
+		}
+		req.getRequestDispatcher("/report/getAll").forward(req, resp);
+	}
+
+
+
+	private void createReport(String subjectName, HttpServletRequest req, HttpServletResponse resp) throws VegaException {
+		Report r = new Report(subjectName, new Date());
+		AllergenService allergenService = vega.getAllergenService();
+
+		Enumeration<String> paramNames = req.getParameterNames();
+		String paramName;
+		String allergenName;
+		while (paramNames.hasMoreElements()) {
+			paramName = paramNames.nextElement();
+			if (paramName.startsWith("Tox")) {
+				allergenName = paramName.substring(3);
+				String strToxValue = req.getParameter(paramName);
+				double toxValue = Double.parseDouble(strToxValue);
+				Allergen a = allergenService.get(allergenName);
+				ToxicityRating tr = new ToxicityRating(a, toxValue);
+				r.addRating(tr);
+			}
+		}
+		vega.getReportService().addReport(r);
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/reportCreation.jsp
@@ -0,0 +1,40 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ page errorPage="showError.jsp" %>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Report</title>
+</head>
+<body>
+<jsp:include page="/WEB-INF/jsp/header.jsp"/>
+<h2>Report</h2>
+<h3>${reportBean.operationLabel}</h3>
+<form action="${contextRoot}/report/do" method="post">
+
+    <p>Subject name <input type="text" name="subjectName"></p>
+    <c:forEach var="category" items="${categories}">
+        <table>
+            <thead>
+            <tr>
+                <th>${category.name}</th>
+            </tr>
+            </thead>
+            <tbody>
+            <c:forEach var="allergen" items="${category.allergens}">
+                <tr>
+                    <td>${allergen.name}</td>
+                    <td><input type="range" name="Tox${allergen.name}" min="0" max="100"></td>
+                </tr>
+            </c:forEach>
+            </tbody>
+        </table>
+    </c:forEach>
+
+    <input type="submit" value="Create"/>
+</form>
+<jsp:include page="/WEB-INF/jsp/footer.jsp"/>
+</body>
+</html>
