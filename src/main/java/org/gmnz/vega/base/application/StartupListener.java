c17364828565cbbe479b6162f159f24f51a8914c
Simone Monotti <s3lvatico@gmail.com>
2018-07-30 14:35:05 +0200

    aggiunta del datasource jndi - da testare

--- src/main/java/org/gmnz/vega/base/application/StartupListener.java
+++ src/main/java/org/gmnz/vega/base/application/StartupListener.java
@@ -1,8 +1,13 @@
 package org.gmnz.vega.base.application;
 
 
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.sql.DataSource;
 
 
 /**
@@ -13,10 +18,17 @@ public class StartupListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		System.out.printf(">>> contextInitialized%n");
-
 		String dummyJndiDataSource = sce.getServletContext().getInitParameter("dummy-jndi-datasource");
 		System.out.printf("dummyJndiDataSource: <%s>%n", dummyJndiDataSource);
-
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource) envContext.lookup("jdbc/vegaDS");
+			Connection conn = ds.getConnection();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.printf("<<< contextInitialized%n");
 	}
 
