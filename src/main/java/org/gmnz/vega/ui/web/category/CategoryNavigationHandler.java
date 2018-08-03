8af8d2c4a8d7cf4bdad5ad51ae54e5d9fa7c0556
Simone Monotti <s3lvatico@gmail.com>
2018-08-03 09:37:48 +0200

    regole di navigazione spostate

--- src/main/java/org/gmnz/vega/ui/web/category/CategoryNavigationHandler.java
+++ src/main/java/org/gmnz/vega/ui/web/category/CategoryNavigationHandler.java
@@ -1,6 +1,43 @@
 package org.gmnz.vega.ui.web.category;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.gmnz.vega.ui.Action;
+
+
 class CategoryNavigationHandler {
 
+	private Map<String, CategoryManagementBean> navigationMap;
+
+
+
+	public CategoryNavigationHandler() {
+		navigationMap = new HashMap<>();
+
+		CategoryManagementBean cmb = new CategoryManagementBean();
+		cmb.setOperationLabel("Registered categories");
+		cmb.setViewName("categories");
+		cmb.setAction(Action.GET_ALL);
+		navigationMap.put("getAll", cmb);
+
+		cmb = new CategoryManagementBean();
+		cmb.setOperationLabel("New Category Creation");
+		cmb.setViewName("categoryManagement");
+		cmb.setAction(Action.CREATE);
+		navigationMap.put("create", cmb);
+
+		cmb = new CategoryManagementBean();
+		cmb.setOperationLabel("Modify Category Name");
+		cmb.setViewName("categoryManagement");
+		cmb.setAction(Action.MODIFY);
+		navigationMap.put("edit", cmb);
+
+		cmb = new CategoryManagementBean();
+		cmb.setOperationLabel("Confirm Category Deletion");
+		cmb.setViewName("categoryDeletion");
+		cmb.setAction(Action.DELETE);
+		navigationMap.put("delete", cmb);
+	}
 }
