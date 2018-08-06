cd0abf74432efb9f52889428e4a2b970abfe4439
Simone Monotti <s3lvatico@gmail.com>
2018-08-06 13:11:42 +0200

    ReportDaoImpl : metodo findAll

--- src/main/java/org/gmnz/vega/repository/ReportDaoImpl.java
+++ src/main/java/org/gmnz/vega/repository/ReportDaoImpl.java
@@ -1,11 +1,13 @@
 package org.gmnz.vega.repository;
 
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.List;
 
 import org.gmnz.vega.domain.Report;
 
@@ -18,12 +20,16 @@ public class ReportDaoImpl extends BasicDaoImpl implements ReportDao {
 		ResultSet rs = null;
 		try {
 			s = connection.createStatement();
-			rs = s.executeQuery("SELECT * FROM report");
-
-			// TODO elabora resultset
-
-			// TODO restituisci il valore corretto
-			return new HashSet<>();
+			rs = s.executeQuery("SELECT * FROM report ORDER BY subject_name, date_creation");
+			List<Report> reports = new ArrayList<>();
+			while (rs.next()) {
+				String id = rs.getString(1);
+				String subjectName = rs.getString(2);
+				Date creationDate = rs.getDate(3);
+				Report r = new Report(id, subjectName, creationDate);
+				reports.add(r);
+			}
+			return reports;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DaoException("ReportDaoImpl.findAll error", e);
