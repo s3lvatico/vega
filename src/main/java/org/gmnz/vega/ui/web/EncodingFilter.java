e8115ef6a78702074f1b5b9a8ee09e5cba074011
Simone Monotti <s3lvatico@gmail.com>
2018-08-06 10:38:22 +0200

    minima revisione del filtro di codifica

--- src/main/java/org/gmnz/vega/ui/web/EncodingFilter.java
+++ src/main/java/org/gmnz/vega/ui/web/EncodingFilter.java
@@ -2,9 +2,6 @@ package org.gmnz.vega.ui.web;
 
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -16,14 +13,14 @@ import javax.servlet.ServletResponse;
 
 public class EncodingFilter implements Filter {
 
-	private String defaultEncoding;
+//	private String defaultEncoding;
 	private String targetEncoding;
 
 
 
 	@Override
 	public void init(FilterConfig filterConfig) {
-		defaultEncoding = filterConfig.getInitParameter("defaultEncoding");
+//		defaultEncoding = filterConfig.getInitParameter("defaultEncoding");
 		targetEncoding = filterConfig.getInitParameter("targetEncoding");
 	}
 
@@ -32,36 +29,15 @@ public class EncodingFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		/*
-		 * String requestCharacterEncoding = request.getCharacterEncoding(); if
-		 * (requestCharacterEncoding == null) { reEncodeRequestParameters(request); }
-		 */
 		request.setCharacterEncoding(targetEncoding);
 		chain.doFilter(request, response);
 	}
 
 
 
-	@SuppressWarnings("unused")
-	@Deprecated
-	private void reEncodeRequestParameters(ServletRequest request) throws UnsupportedEncodingException {
-		Map<String, String> parametersMap = new HashMap<>();
-		String paramName;
-		String paramValue;
-
-		while (request.getParameterNames().hasMoreElements()) {
-			paramName = request.getParameterNames().nextElement();
-			paramValue = request.getParameter(paramName);
-			paramValue = new String(paramValue.getBytes(defaultEncoding), targetEncoding);
-			parametersMap.put(paramName, paramValue);
-		}
-		request.setAttribute("parametersMap", parametersMap);
-	}
-
-
-
 	@Override
 	public void destroy() {
-
+		// nessuna operazione necessaria
 	}
+
 }
