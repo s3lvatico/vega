1585014715f9530769e4823abff8433f69b2bed8
S3lvatico <s3lvatico@gmail.com>
2018-08-18 12:00:51 +0200

    pulizia codice commentato

--- src/main/java/org/gmnz/vega/repository/AllergenDaoImpl.java
+++ src/main/java/org/gmnz/vega/repository/AllergenDaoImpl.java
@@ -5,7 +5,6 @@ import org.gmnz.vega.domain.Allergen;
 import org.gmnz.vega.domain.Category;
 import org.springframework.jdbc.core.RowMapper;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -40,31 +39,6 @@ class AllergenDaoImpl extends BasicDaoImpl implements AllergenDao {
 								+ " ORDER BY category_name ";
 //@formatter:on
 		return jdbcTemplate.query(sqlQuery, new AllergenRowMapper());
-
-// 		Statement s = null;
-//		ResultSet rs = null;
-//		try {
-//			s = connection.createStatement();
-//			rs = s.executeQuery("SELECT allrg.id, allrg.e_name allergen_name, cat.e_name category_name \n"
-//					+ " FROM category cat\n" + " JOIN allergen allrg ON allrg.id_category = cat.id\n"
-//					+ " WHERE allrg.deleted = 0" + " ORDER BY category_name");
-//			ArrayList<Allergen> allergens = new ArrayList<>();
-//			while (rs.next()) {
-//				String id = rs.getString(1);
-//				String allergenName = rs.getString("allergen_name");
-//				Allergen a = new Allergen(allergenName);
-//				a.setId(id);
-//				String categoryName = rs.getString("category_name");
-//				a.setCategory(new Category(categoryName));
-//				allergens.add(a);
-//			}
-//			return allergens;
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			throw new DaoException("AllergenDaoImpl.findAll error", e);
-//		} finally {
-//			releaseResources(s, rs);
-//		}
 	}
 
 
@@ -98,32 +72,7 @@ class AllergenDaoImpl extends BasicDaoImpl implements AllergenDao {
 						"join category on allergen.id_category = category.id and allergen.deleted = 0 " +
 						"where allergen.id = ? ";
 //formatter:on
-		return jdbcTemplate.queryForObject(sqlQuery, new Object[] {id}, new AllergenByIdRowMapper());
-//		PreparedStatement s = null;
-//		ResultSet rs = null;
-//		try {
-//			s = connection.prepareStatement("select " + " allergen.id allergen_id, " + " allergen.e_name allergen_name, "
-//					+ " category.id category_id, " + " category.e_name category_name " + " from allergen "
-//					+ " join category on " + " allergen.id_category = category.id and allergen.deleted = 0 "
-//					+ " where  allergen.id = ?");
-//			s.setString(1, id);
-//			rs = s.executeQuery();
-//
-//			Allergen a = null;
-//			if (rs.next()) {
-//				a = new Allergen(rs.getString("allergen_name"));
-//				a.setId(rs.getString("allergen_id"));
-//				Category c = new Category(rs.getString("category_name"));
-//				c.setId(rs.getString("category_id"));
-//				a.setCategory(c);
-//			}
-//			return a;
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			throw new DaoException("AllergenDaoImpl.findById error", e);
-//		} finally {
-//			releaseResources(s, rs);
-//		}
+		return jdbcTemplate.queryForObject(sqlQuery, new Object[]{id}, new AllergenByIdRowMapper());
 	}
 
 
@@ -137,22 +86,6 @@ class AllergenDaoImpl extends BasicDaoImpl implements AllergenDao {
 		} else {
 			return count == 1;
 		}
-
-//		PreparedStatement ps = null;
-//		ResultSet rs = null;
-//		try {
-//			ps = connection.prepareStatement("SELECT COUNT(*) FROM allergen WHERE e_name = ? AND deleted = '0'");
-//			ps.setString(1, name);
-//			rs = ps.executeQuery();
-//			rs.next(); // deve esser fatto per forza
-//			int countValue = rs.getInt(1);
-//			return countValue == 1;
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			throw new DaoException("AllergenDaoImpl.isAllergenRegisteredByName error", e);
-//		} finally {
-//			releaseResources(ps, rs);
-//		}
 	}
 
 
@@ -162,19 +95,6 @@ class AllergenDaoImpl extends BasicDaoImpl implements AllergenDao {
 		String sqlStatement = "INSERT  INTO  allergen VALUES (?, ?, 0, ?)";
 		Object[] params = {UUID.randomUUID().toString(), allergen.getName(), allergen.getCategory().getId()};
 		jdbcTemplate.update(sqlStatement, params);
-//		PreparedStatement s = null;
-//		try {
-//			s = connection.prepareStatement("INSERT  INTO  allergen VALUES (?, ?, 0, ?)");
-//			s.setString(1, UUID.randomUUID().toString());
-//			s.setString(2, allergen.getName());
-//			s.setString(3, allergen.getCategory().getId());
-//			s.executeUpdate();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			throw new DaoException("AllergenDaoImpl.create error", e);
-//		} finally {
-//			releaseResources(s);
-//		}
 	}
 
 
@@ -184,20 +104,6 @@ class AllergenDaoImpl extends BasicDaoImpl implements AllergenDao {
 		String sqlStatement = "update allergen set e_name = ?, id_category = ? where id = ? and deleted = 0";
 		Object[] updateParams = {allergen.getName(), allergen.getCategory().getId(), allergen.getId()};
 		jdbcTemplate.update(sqlStatement, updateParams);
-//		PreparedStatement s = null;
-//		try {
-//			s = connection
-//					.prepareStatement("update allergen set e_name = ?, id_category = ? where id = ? and deleted = 0");
-//			s.setString(1, allergen.getName());
-//			s.setString(2, allergen.getCategory().getId());
-//			s.setString(3, allergen.getId());
-//			s.executeUpdate();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			throw new DaoException("AllergenDaoImpl.update error", e);
-//		} finally {
-//			releaseResources(s);
-//		}
 	}
 
 
@@ -206,17 +112,6 @@ class AllergenDaoImpl extends BasicDaoImpl implements AllergenDao {
 	public void delete(String id) throws DaoException {
 		String sqlStatement = "update allergen set deleted = 1 where id = ?";
 		jdbcTemplate.update(sqlStatement, id);
-//		PreparedStatement s = null;
-//		try {
-//			s = connection.prepareStatement("update allergen set deleted = 1 where id = ?");
-//			s.setString(1, id);
-//			s.executeUpdate();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			throw new DaoException("AllergenDaoImpl.delete error", e);
-//		} finally {
-//			releaseResources(s);
-//		}
 	}
 
 }
