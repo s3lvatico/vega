6708366b4ce470ffeaf0483ae6fbf66569809db5
Simone Monotti <s3lvatico@gmail.com>
2018-08-27 12:32:33 +0200

    CategoryDao : metodo pericoloso indicato come deprecato

--- src/main/java/org/gmnz/vega/repository/CategoryDao.java
+++ src/main/java/org/gmnz/vega/repository/CategoryDao.java
@@ -1,30 +1,48 @@
 package org.gmnz.vega.repository;
 
 
-import org.gmnz.vega.domain.Category;
-
 import java.util.List;
+
+import org.gmnz.vega.domain.Category;
 
 
 public interface CategoryDao /* extends ConnectionOrientedDao */ {
 
 	List<Category> findAll() throws DaoException;
 
+
+
 	List<Category> findAllWithAllergens() throws DaoException;
+
+
 
 	Category findById(String id) throws DaoException;
 
+
+
 	Category findByName(String name, boolean deleted) throws DaoException;
+
+
 
 	String create(String name) throws DaoException;
 
+
+
 	void update(Category category) throws DaoException;
+
+
 
 	int countAllergens(String categoryId) throws DaoException;
 
+
+
 	void delete(String categoryId) throws DaoException;
 
+
+
 	boolean isCategoryRegisteredByName(String name) throws DaoException;
+
+
 
 	/**
 	 * @deprecated non si usa più
@@ -35,10 +53,17 @@ public interface CategoryDao /* extends ConnectionOrientedDao */ {
 	@Deprecated
 	boolean isCategoryRegisteredById(String categoryId) throws DaoException;
 
+
+
 	/**
-	 * Elimina fisicame
+	 * Elimina fisicamente un record.
+	 * 
+	 * Questo può introdurre inconsistenze.
+	 * 
+	 * @deprecated da non usare e da rimuovere in futuro
 	 * @param categoryId
 	 * @throws DaoException
 	 */
+	@Deprecated
 	void deepDelete(String categoryId) throws DaoException;
 }
