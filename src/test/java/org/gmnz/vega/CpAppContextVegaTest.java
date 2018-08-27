91a67c699085ff6d34a12def3209fa88236e784b
Simone Monotti <s3lvatico@gmail.com>
2018-08-27 08:53:17 +0200

    context spring usato come risorsa e chiuso correttamente

--- src/test/java/org/gmnz/vega/CpAppContextVegaTest.java
+++ src/test/java/org/gmnz/vega/CpAppContextVegaTest.java
@@ -1,28 +1,30 @@
 package org.gmnz.vega;
 
-import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class CpAppContextVegaTest {
 
 	@Test
 	public void dummyTest() throws SQLException {
-		System.out.println("solo un test che non fallisce");
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml");
-		System.out.println("ctx.getApplicationName() : " + ctx.getApplicationName());
-		System.out.println("ctx.getDisplayName()     : " + ctx.getDisplayName());
-		System.out.println("ctx.getId()              : " + ctx.getId());
+		try (ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("/applicationContext.xml")) {
+			System.out.println("ctx.getApplicationName() : " + ctx.getApplicationName());
+			System.out.println("ctx.getDisplayName()     : " + ctx.getDisplayName());
+			System.out.println("ctx.getId()              : " + ctx.getId());
 
-		DataSource ds = (DataSource) ctx.getBean("dataSource");
-		Connection c = ds.getConnection();
-		System.out.println("connection = " + c);
-		// non è necessario chiudere esplicitamente la connessione
-		// perché viene chiusa automaticamente dal dataSource
-		// c.close();
+			DataSource ds = (DataSource) ctx.getBean("dataSource");
+			Connection c = ds.getConnection();
+			System.out.println("connection = " + c);
+
+		} finally {
+			System.out.println("done");
+		}
 	}
 }
