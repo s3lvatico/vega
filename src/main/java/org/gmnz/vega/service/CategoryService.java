a89bc6ffbce219b1c4b21902195452de4ef4edbf
Simone Monotti <s3lvatico@gmail.com>
2018-08-27 12:30:16 +0200

    CategoryService.deepRemove è pericoloso ed è marcato come deprecato

--- src/main/java/org/gmnz/vega/service/CategoryService.java
+++ src/main/java/org/gmnz/vega/service/CategoryService.java
@@ -1,10 +1,10 @@
 package org.gmnz.vega.service;
 
 
+import java.util.List;
+
 import org.gmnz.vega.VegaException;
 import org.gmnz.vega.domain.Category;
-
-import java.util.List;
 
 
 /**
@@ -15,20 +15,34 @@ public interface CategoryService {
 	List<Category> getAllCategories() throws VegaException;
 
 
+
 	List<Category> getAllCategoriesWithAllergens() throws VegaException;
+
 
 
 	Category getCategoryById(String id) throws VegaException;
 
 
+
 	String createCategory(String name) throws VegaException;
+
 
 
 	void changeCategoryName(String categoryId, String newCategoryName) throws VegaException;
 
 
+
 	void removeCategory(String id) throws VegaException;
 
+
+
+	/**
+	 * @deprecated è sbagliato introdurre metodi da usare solo per scopi di test in
+	 *             un'interfaccia applicativa
+	 * @param id id della categoria da rimuovere
+	 * @throws VegaException se qualcosa va storto
+	 */
+	@Deprecated
 	void deepRemove(String id) throws VegaException;
 
 }
