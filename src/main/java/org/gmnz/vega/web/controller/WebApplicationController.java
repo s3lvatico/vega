c824cbc3a9f8cf0d154e9521ef42d8f45be25530
S3lvatico <s3lvatico@gmail.com>
2018-09-07 08:31:12 +0200

    fai 'sto santo command dai

--- src/main/java/org/gmnz/vega/web/controller/WebApplicationController.java
+++ src/main/java/org/gmnz/vega/web/controller/WebApplicationController.java
@@ -1,13 +1,21 @@
 package org.gmnz.vega.web.controller;
 
+
 import org.gmnz.vega.web.context.RequestContext;
 import org.gmnz.vega.web.context.ResponseContext;
+
 
 class WebApplicationController implements ApplicationController {
 
 	@Override
 	public ResponseContext handleRequest(RequestContext requestContext) {
 		// TODO Auto-generated method stub
+		/*
+		chiama la command factory
+		crea il comando sulla base del nome (sta nel requestContext)
+		inietta nel comando i parametri
+		esegui il comando
+		 */
 		return null;
 	}
 
@@ -16,7 +24,9 @@ class WebApplicationController implements ApplicationController {
 	@Override
 	public void handleResponse(RequestContext requestContext, ResponseContext responseContext) {
 		// TODO Auto-generated method stub
-
+		/*
+		da richiesta e risposta determina una vista
+		 */
 	}
 
 }
