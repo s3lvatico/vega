707c8abe63742b27c498703c57cbe22417195e96
Simone Monotti <s3lvatico@gmail.com>
2018-09-11 13:18:02 +0200

    INSTABILE - finire di sistemare i contextobject

--- src/main/java/org/gmnz/vega/web/context/VegaHttpResponseContext.java
+++ src/main/java/org/gmnz/vega/web/context/VegaHttpResponseContext.java
@@ -1,9 +1,6 @@
 package org.gmnz.vega.web.context;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,66 +8,31 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * creato da simone in data 08/09/2018.
  */
-class VegaHttpResponseContext implements ResponseContext {
-
-	private final Map<String, String> parameters;
-	private final Map<String, Object> attributes;
-
-
+class VegaHttpResponseContext extends AbstractContextObject implements ResponseContext {
 
 	VegaHttpResponseContext() {
-		parameters = new HashMap<>();
-		attributes = new HashMap<>();
-		attributes.put(OUTCOME, 200);
+		setAttribute(OUTCOME, 200);
 	}
 
 
 
 	@Override
 	public int getOutcome() {
-		return (Integer) attributes.get(ResponseContext.OUTCOME);
+		return (Integer) getAttribute(ResponseContext.OUTCOME);
 	}
 
 
 
 	@Override
 	public void setRequest(HttpServletRequest request) {
-		attributes.put(ResponseContext.ORIGINAL_REQUEST, request);
+		setAttribute(ResponseContext.ORIGINAL_REQUEST, request);
 	}
 
 
 
 	@Override
 	public void setResponse(HttpServletResponse response) {
-		attributes.put(ResponseContext.ORIGINAL_RESPONSE, response);
-	}
-
-
-
-	@Override
-	public void setParameter(String name, String value) {
-		parameters.put(name, value);
-	}
-
-
-
-	@Override
-	public void setAttribute(String name, Object value) {
-		attributes.put(name, value);
-	}
-
-
-
-	@Override
-	public String getParameter(String name) {
-		return parameters.get(name);
-	}
-
-
-
-	@Override
-	public Object getAttribute(String name) {
-		return attributes.get(name);
+		setAttribute(ResponseContext.ORIGINAL_RESPONSE, response);
 	}
 
 }
