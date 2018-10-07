72b77922280c27bab6cc429e16a04bce1b0d0e53
S3lvatico <s3lvatico@gmail.com>
2018-10-07 21:18:59 +0200

    hsql ora viene eseguito in una finestra a parte

--- db/run-hsql-server.bat
+++ db/run-hsql-server.bat
@@ -1,2 +1,2 @@
 @echo off
-java -cp ..\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:vegadb --dbname.0 vega
+start java -cp ..\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:vegadb --dbname.0 vega
